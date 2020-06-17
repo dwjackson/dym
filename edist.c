784ec18bb801c8b86dcfbfd87f167bfe19ec2289
David Jackson <dwjackson@users.noreply.github.com>
2020-06-17 17:52:33 -0400

    Add edist test for equal length strings, 1 swap

--- edist.c
+++ edist.c
@@ -5,6 +5,11 @@ int dym_edist(const char *s1, const char *s2)
 {
 	size_t s1_len = strlen(s1);
 	size_t s2_len = strlen(s2);
+	size_t i;
+	int distance = 0;
+	size_t len = s1_len > s2_len ? s1_len : s2_len;
+	char ch1, ch2;
+
 	if (s1_len == 0 && s2_len == 0) {
 		return 0;
 	}
@@ -14,5 +19,13 @@ int dym_edist(const char *s1, const char *s2)
 	if (s2_len == 0) {
 		return s1_len;
 	}
-	return -1;
+	for (i = 0; i < len; i++) {
+		ch1 = s1[i];
+		ch2 = s2[i];
+		if (ch1 != ch2) {
+			distance++;
+		}
+	}
+
+	return distance;
 }
--- test_edist.c
+++ test_edist.c
@@ -18,12 +18,20 @@ ASTRO_TEST_BEGIN(test_empty_vs_nonempty)
 }
 ASTRO_TEST_END
 
+ASTRO_TEST_BEGIN(test_single_swap)
+{
+	int dist = dym_edist("abc", "azc");
+	assert_int_eq(1, dist, "Strings should have 1 distance, swap");
+}
+ASTRO_TEST_END
+
 int main()
 {
 	int num_failures = 0;
 	struct astro_suite *suite = astro_suite_create();
 	astro_suite_add_test(suite, test_empty_strings, NULL);
 	astro_suite_add_test(suite, test_empty_vs_nonempty, NULL);
+	astro_suite_add_test(suite, test_single_swap, NULL);
 	num_failures = astro_suite_run(suite);
 	astro_suite_destroy(suite);
 	return num_failures == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
