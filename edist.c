a82e2420f9c20639de7955e77000ea227c27d4b0
David Jackson <dwjackson@users.noreply.github.com>
2020-06-17 17:45:14 -0400

    Add test for edist of empty v. non-empty strings

--- edist.c
+++ edist.c
@@ -1,8 +1,18 @@
 #include "dym.h"
+#include <string.h>
 
 int dym_edist(const char *s1, const char *s2)
 {
-	(void)s1;
-	(void)s2;
-	return 0;
+	size_t s1_len = strlen(s1);
+	size_t s2_len = strlen(s2);
+	if (s1_len == 0 && s2_len == 0) {
+		return 0;
+	}
+	if (s1_len == 0) {
+		return s2_len;
+	}
+	if (s2_len == 0) {
+		return s1_len;
+	}
+	return -1;
 }
--- test_edist.c
+++ test_edist.c
@@ -9,11 +9,21 @@ ASTRO_TEST_BEGIN(test_empty_strings)
 }
 ASTRO_TEST_END
 
+ASTRO_TEST_BEGIN(test_empty_vs_nonempty)
+{
+	int dist = dym_edist("", "abc");
+	assert_int_eq(3, dist, "Distance was not length of non-empty string");
+	dist = dym_edist("abc", "");
+	assert_int_eq(3, dist, "Distance was not length of non-empty string");
+}
+ASTRO_TEST_END
+
 int main()
 {
 	int num_failures = 0;
 	struct astro_suite *suite = astro_suite_create();
 	astro_suite_add_test(suite, test_empty_strings, NULL);
+	astro_suite_add_test(suite, test_empty_vs_nonempty, NULL);
 	num_failures = astro_suite_run(suite);
 	astro_suite_destroy(suite);
 	return num_failures == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
