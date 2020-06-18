d2bc536ebb031650499fcc7a78fdf0e0648c864f
David Jackson <dwjackson@users.noreply.github.com>
2020-06-17 21:30:23 -0400

    Implement loading strings from file (vs. stdin)

--- dym.c
+++ dym.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "dym.h"
 
 #define USAGE_FMT "USAGE: %s [ARGS...] [STRING]\n"
@@ -16,12 +17,31 @@ int main(int argc, char *argv[])
 	int dist;
 	char *input;
 	size_t line_len;
+	int opt;
+	extern int optind;
+	extern char *optarg;
+	char *filename;
 
 	if (argc < 2) {
 		printf(USAGE_FMT, argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	input = argv[1];
+	while ((opt = getopt(argc, argv, "f:")) != -1) {
+		switch (opt) {
+			case 'f':
+				filename = optarg;
+				fp = fopen(filename, "r");
+				if (fp == NULL) {
+					perror(filename);
+					exit(EXIT_FAILURE);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	input = argv[optind];
 
 	while (fgets(line, LINE_BUFSIZE, fp) != NULL) {
 		line_len = strlen(line);
@@ -37,6 +57,8 @@ int main(int argc, char *argv[])
 		}
 	}
 	printf("%s\n", closest);
+
+	fclose(fp);
 
 	return 0;
 }
